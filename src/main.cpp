0159f5df841f95224d46cef09fabae81eca7249e
tuokri <tuokri@tuta.io>
2017-04-16 15:37:50 +0300

    barebone I/O

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,53 @@
  * \brief Provides command line interface.
  */
 
+#include <ios>
 #include <iostream>
+#include <sstream>
+#include <exception>
+#include <stdexcept>
+
 #include "vigenere_square.hpp"
 
 /**
  * \brief Main function for user I/O and error handling.
+ * \return 0 if no errors.
  */
 int main()
 {
-    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    vigenere_square vsq{alphabet};
-
     std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
     std::cout << "Vigenère cipher demo using the Vigenère table." << std::endl;
     std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+
+    std::string alphabet;
+    std::string cipher;
+    std::string key;
+    std::stringstream ss;
+
+    std::cout << "Input alphabet and press ENTER. Input '_DEFAULT_' " <<
+                 "to use default alphabet." << std::endl;
+    std::cin >> std::skipws >> alphabet;
+
+    if(alphabet == "_DEFAULT_")
+    {
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+    }
+
+    try
+    {
+        vigenere_square vsq{alphabet};
+    }
+    catch(std::invalid_argument e)
+    {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+
+    std::cout << "Input cipher and press ENTER." << std::endl;
+    std::cin >> cipher;
+
+    std::cout << "Input key and press ENTER." << std::endl;
+    std::cin >> key;
 
     return 0;
 }
