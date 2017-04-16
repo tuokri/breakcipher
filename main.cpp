8e58e458f3026200c7a5e845d193959212420e29
tuokri <tuokri@tuta.io>
2017-04-16 11:06:42 +0300

    fancier prints

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
+/**
+ * \file main.cpp
+ * \brief Provides command line interface.
+ */
+
 #include <iostream>
 #include "vigenere_square.hpp"
 
+/**
+ * \brief Main function for user I/O and error handling.
+ */
 int main()
 {
     std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     vigenere_square vsq{alphabet};
 
-    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
-    std::cout << "Vigenère cipher demo using Vigenère table." << std::endl;
-    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "Vigenère cipher demo using the Vigenère table." << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
 
     return 0;
 }
