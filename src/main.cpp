fff4b31894f9d12aa8625c5318dfbc4396beea1b
tuokri <tuokri@tuta.io>
2017-04-19 21:44:44 +0300

    command line interface

--- src/main.cpp
+++ src/main.cpp
@@ -22,23 +22,25 @@ int main()
     std::cout << "Vigenère cipher demo using the Vigenère table." << std::endl;
     std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
 
+    vigenere_square vsq;
     std::string alphabet;
-    std::string cipher;
+    std::string text;
     std::string key;
     std::stringstream ss;
 
     std::cout << "Input alphabet and press ENTER. Input '_DEFAULT_' " <<
                  "to use default English alphabet." << std::endl;
-    std::cin >> std::skipws >> alphabet;z
+    std::cin >> std::skipws >> alphabet;
 
     if(alphabet == "_DEFAULT_")
     {
-        alphabet = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     }
 
     try
     {
-        vigenere_square vsq{stringutils::uppercase(alphabet)};
+        vsq = vigenere_square{stringutils::uppercase(alphabet)};
     }
     catch(std::invalid_argument e)
     {
@@ -46,11 +48,41 @@ int main()
         return 1;
     }
 
-    std::cout << "Input cipher and press ENTER." << std::endl;
-    std::cin >> cipher;
+    std::string prompt;
+    bool encrypt;
+    while(true)
+    {
+        std::cout << "Please choose 'encrypt' or 'decrypt' and press ENTER." << std::endl;
+        std::cin >> std::skipws >> text;
+        text = stringutils::uppercase(text);
+        if(text == "ENCRYPT")
+        {
+            prompt =  "Input plain text and press ENTER.";
+            encrypt = true;
+            break;
+        }
+        else if(text == "DECRYPT")
+        {
+            prompt =  "Input cipher text and press ENTER.";
+            encrypt = false;
+            break;
+        }
+    }
+
+    std::cout << prompt << std::endl;
+    std::cin >> std::skipws >> text;
 
     std::cout << "Input key and press ENTER." << std::endl;
-    std::cin >> key;
+    std::cin >> std::skipws >> key;
+
+    if(encrypt)
+    {
+        std::cout << vsq.encrypt(text, key) << std::endl;
+    }
+    else
+    {
+        std::cout << vsq.decrypt(text, key) << std::endl;
+    }
 
     return 0;
 }
