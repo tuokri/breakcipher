ad12c319c2c7ff3cd5606fd7c4d71f91600fd78a
tuokri <tuokri@tuta.io>
2017-04-19 17:05:23 +0300

    added docs

--- src/stringutils.hpp
+++ src/stringutils.hpp
@@ -10,10 +10,27 @@
 
 namespace stringutils
 {
+    /**
+     * \brief Circular shift (rotate) of string.
+     * \param str Reference to string shift.
+     * \param i How many times to shif the string.
+              Negative values indicate left shift, positives right shift.
+     * \return A copy of the original string, shifted.
+     */
     std::string shift(const std::string& str, int i);
 
+    /**
+     * \brief Copies a string and converts the copy to uppercase.
+     * \param str Reference to string to convert.
+     * \return Uppercase copy of the original string.
+     */
     std::string uppercase(const std::string& str);
 
+    /**
+     * \brief Checks if string contains all unique characters (no duplicates).
+     * \param str Reference to string to check for uniqueness.
+     * \return true if all unique, else false.
+     */
     bool is_all_unique(const std::string& str);
 }
 
